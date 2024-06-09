c2d4ecfdb87aea030954b42f8cc4517667984ffb
lonelinessmap <160585338+lonelinessmap@users.noreply.github.com>
2024-06-09 10:01:16 +0300

    Create team.js

---
+++ team.js
@@ -0,0 +1,36 @@
+function displayTeamContent() {
+    worldMap.style.display = "none";
+    homeButton.style.display = "block";
+    
+    const teamMembers = [
+        {
+            name: "Mowafa",
+            title: "Professor",
+            bio: "he is currently a professor at HBKU",
+            photo: "mowafa.jpg"
+        },
+        {
+            name: "Hurmat",
+            title: "Postdoc",
+            bio: "he is currently a postdoc at HBKU.",
+            photo: "hurmat.jpg"
+        },
+        {
+            name: "Atiq",
+            title: "Software Engineer",
+            bio: "he is currently a software engineer at HBKU.",
+            photo: "atiq.png"
+        }
+    ];
+
+    const teamHtml = teamMembers.map(member => `
+        <div class="team-member">
+            <img src="${member.photo}" alt="${member.name}" class="team-photo">
+            <h3>${member.name}</h3>
+            <h4>${member.title}</h4>
+            <p>${member.bio}</p>
+        </div>
+    `).join('');
+
+    contentDiv.innerHTML = `<h2>Team</h2>${teamHtml}`;
+}
